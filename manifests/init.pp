bc097ae398299cf5c36414bafb78d52dda7ddb99
Walter Gildersleeve <walter@gildersleeve.eu>
2018-01-29 16:55:36 +0100

    added user creation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,12 @@
 class accman {
+  user { 'wmg':
+  ensure => 'present',
+  home => '/home/wmg',
+  password => 'puppet',
+  shell => '/bin/bash',
+  groups => 'wmg',
+  comment => 'Walter Gildersleeve'
+  }
+  group { 'wmg':
+  }
 }
