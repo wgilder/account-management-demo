5f2c8dd83c3505e83df496a4460cb803ce257d33
Walter Gildersleeve <walter@gildersleeve.eu>
2018-01-29 18:06:47 +0100

    fixed home directory and group

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,25 @@
 class accman {
   user { 'wmg':
-  ensure => 'present',
-  home => '/home/wmg',
-  password => 'puppet',
-  shell => '/bin/bash',
-  groups => 'wmg',
-  comment => 'Walter Gildersleeve'
+    ensure           => 'present',
+    comment          => 'Walter Gildersleeve',
+    gid              => 1002,
+    groups           => ['wmg'],
+    home             => '/home/wmg',
+    password         => '$1$7ssdE1IM$Q7xtFGUS3Ckzvnskx.rU61',
+    password_max_age => 99999,
+    password_min_age => 0,
+    shell            => '/bin/bash',
+    uid              => 1011,
+  }  
+  
+  file { '/home/wmg':
+    ensure => 'directory',
+    owner  => 'wmg',
+    group  => 'wmg',
+    mode   => '0700',
   }
+
   group { 'wmg':
-  ensure => 'present',
+    ensure => 'present',
   }
 }
